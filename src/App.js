f1e0bb9b48204fd2cd36c598f972c5cb48efe9e0
eugenia1984 <mariaeugeniacosta1@gmail.com>
2022-10-02 20:02:53 -0300

    agregamos react-router-dom y las paginas con las rutas

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/header/Header.jsx";
+import Footer from "./components/footer/Footer.jsx";
+import Home from "./pages/Home.jsx";
+import Productos from "./pages/Productos.jsx";
+import Contacto from "./pages/Contacto.jsx";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hola</h1>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/productos" element={<Productos />} />
+        <Route path="/contacto" element={<Contacto />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
