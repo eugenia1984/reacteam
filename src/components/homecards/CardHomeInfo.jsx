5b77eb4458d30350a5694c3ad3330faacbf9c1fb
eugenia1984 <mariaeugeniacosta1@gmail.com>
2022-11-16 12:45:18 -0300

    ajuste las cards para que queden todas del mismo alto y no se deforme la imagen

--- src/components/homecards/CardHomeInfo.jsx
+++ src/components/homecards/CardHomeInfo.jsx
@@ -1,13 +1,13 @@
 const cardHomeInfo = [
   {
     id: 1,
-    image:"https://placehold.co/300x210", 
+    image:"https://res.cloudinary.com/dtq1qaw4z/image/upload/v1668610984/alta-pinta/bermuda-rustico1_qwgivo.webp", 
     title: "Pantalones", 
     btntext: "Ver"
   },
   {
     id: 2,
-    image:"https://placehold.co/308x210", 
+    image:"https://res.cloudinary.com/dtq1qaw4z/image/upload/v1668610998/alta-pinta/remera-azul1_byil0b.webp", 
     title: "Remeras y Polos", 
     btntext: "Ver"
   },
@@ -25,13 +25,13 @@ const cardHomeInfo = [
   },
   {
     id: 5,
-    image:"https://placehold.co/308x210", 
+    image:"https://res.cloudinary.com/dtq1qaw4z/image/upload/v1668610997/alta-pinta/camisa-blanca3_c248sv.webp", 
     title: "Camisas", 
     btntext: "Ver"
   },
   {
     id: 6,
-    image:"https://placehold.co/308x210", 
+    image:"https://res.cloudinary.com/dtq1qaw4z/image/upload/v1668610997/alta-pinta/campera1_g83qxn.webp", 
     title: "Sweaters y Cardigans", 
     btntext: "Ver"
   }
--- src/components/homecards/CardListItem.jsx
+++ src/components/homecards/CardListItem.jsx
@@ -1,5 +1,6 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import "./CardStyle.css";
 
 export const CardListItem = props => {
   return (
@@ -8,10 +9,12 @@ export const CardListItem = props => {
         src={props.cardHomeInfo.image } 
         alt={props.cardHomeInfo.title} />
       <Card.ImgOverlay>
-        <Card.Title>
+        <Card.Title
+          className="mt-5 bg-dark bg-opacity-25 py-2 px-2">
           {props.cardHomeInfo.title}
         </Card.Title>
-        <Card.Text>
+        <Card.Text
+          className="pb-2 px-2 bg-dark bg-opacity-25">
           Last updated 3 mins ago
         </Card.Text>
         <Button variant="dark">
