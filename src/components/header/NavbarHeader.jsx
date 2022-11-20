d0573c456fa1e6cea85cfca14df2c45cff793300
vaniwinnik <vanina.winnik@argano.com>
2022-11-20 17:31:48 -0300

    Agrego link al Navbar

--- src/components/header/NavbarHeader.jsx
+++ src/components/header/NavbarHeader.jsx
@@ -1,6 +1,7 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import { Link } from "react-router-dom"
 import Logo from "./Logo.jsx";
 import ShoppingCart from "../shoppingcart/ShoppingCart.jsx";
 
@@ -15,9 +16,9 @@ function NavbarHeader() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="productos">Productos</Nav.Link>
-            <Nav.Link href="contacto">Contacto</Nav.Link>
+            <Link className="nav-link link-light" to="/">Home</Link>
+            <Link className="nav-link link-light" to="productos">Productos</Link>
+            <Link className="nav-link link-light" to="contacto">Contacto</Link>
           </Nav>
         </Navbar.Collapse>
         <ShoppingCart />
