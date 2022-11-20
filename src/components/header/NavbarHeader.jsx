0756c2e67ca930160f135032ce5505b4ef42cf66
vaniwinnik <vanina.winnik@argano.com>
2022-11-20 15:37:15 -0300

    Elimino código innecesario

--- src/components/header/NavbarHeader.jsx
+++ src/components/header/NavbarHeader.jsx
@@ -1,7 +1,6 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
 import Logo from "./Logo.jsx";
 import ShoppingCart from "../shoppingcart/ShoppingCart.jsx";
 
@@ -17,21 +16,7 @@ function NavbarHeader() {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="/">Home</Nav.Link>
-            <NavDropdown title="Productos" id="basic-nav-dropdown">
-              <NavDropdown.Item href="/productos/pantalones">
-                Pantalones
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="/productos/remeras">
-                Remeras
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="/productos/buzos">Buzos</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="/productos/calzado">
-                Calzado
-              </NavDropdown.Item>
-            </NavDropdown>
+            <Nav.Link href="productos">Productos</Nav.Link>
             <Nav.Link href="contacto">Contacto</Nav.Link>
           </Nav>
         </Navbar.Collapse>
