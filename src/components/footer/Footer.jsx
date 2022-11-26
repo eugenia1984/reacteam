61d8e8207e86c058ba8f5561170de90ba0c31c8e
eugenia1984 <mariaeugeniacosta1@gmail.com>
2022-11-26 11:26:48 -0300

    modifico que tanto los links del footer como los de las redes sociales se rendericen mapeandolos

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,89 +1,81 @@
 import "./footer.css";
+import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faTwitter,
-  faDribbble,
   faFacebook,
   faLinkedin,
 } from "@fortawesome/free-brands-svg-icons";
 
 function Footer() {
+  const categories = [
+    {
+      categoria: "Pantalones",
+      link:"pantalones"
+    },
+    {
+      categoria: "Remeras y polos",
+      link: "remeras-y-polos"    
+    },
+    {
+      categoria: "Abrigos y camperas",
+      link: "abrigos-y-camperas"
+    },
+    {
+      categoria: "Calzados",
+      link: "calzados"
+    },
+    {
+      categoria: "Camisas",
+      link: "camisas"
+    },
+    {
+      categoria: "Sweaters y cardigans",
+      link: "sweaters-y-cardigans"
+    }
+  ];
+
+  const socialMedia = [
+    {
+      href: "https://github.com/VaninaWinnik/reacteam",
+      icon: {faFacebook},
+    },
+    {
+      href: "https://github.com/VaninaWinnik/reacteam",
+      icon: {faTwitter},
+    },
+    {
+      href: "https://github.com/VaninaWinnik/reacteam",
+      icon: {faLinkedin},
+    }
+  ]
   return (
     <footer className="site-footer bg-dark text-light">
       <div className="container">
         <div className="row ">
-          <div className="col-xs-6 col-md-6">
+          <div className="col-xs-12 col-sm-7 col-md-9 col-lg-10">
             <h6>Categorias</h6>
             <ul className="footer-links d-flex justify-content-between">
-              <li>
-                <a href="http://scanfcode.com/category/c-language/">
-                  Pantalones
-                </a>
-              </li>
-              <li>
-                <a href="http://scanfcode.com/category/front-end-development/">
-                  Remeras
-                </a>
-              </li>
-              <li>
-                <a href="http://scanfcode.com/category/back-end-development/">
-                  Buzos
-                </a>
-              </li>
-              <li>
-                <a href="http://scanfcode.com/category/java-programming-language/">
-                  Calzado
-                </a>
-              </li>
+              {categories.map((categorie, index) => (
+                <li key={index}>
+                  <Link to={`/productos/${categorie.link}`}>{categorie.categoria}</Link>
+                </li>
+              ))}
             </ul>
           </div>
-
-          <div className="col-md-4 col-sm-6 col-xs-12">
+          <div className="col-xs-12 col-sm-5 col-md-3 col-lg-2">
             <ul className="social-icons">
-              <li>
-                <a
-                  href="https://facebook.com"
-                  target={"_blank"}
-                  rel="noreferrer"
-                >
-                  <i>
-                    <FontAwesomeIcon icon={faFacebook} />
-                  </i>
-                </a>
-              </li>
-              <li>
-                <a
-                  href="https://twitter.com"
-                  target={"_blank"}
-                  rel="noreferrer"
-                >
-                  <i>
-                    <FontAwesomeIcon icon={faTwitter} />
-                  </i>
-                </a>
-              </li>
-              <li>
-                <a
-                  href="https://dribbble.com"
-                  target={"_blank"}
-                  rel="noreferrer"
-                >
-                  <i>
-                    <FontAwesomeIcon icon={faDribbble} />
-                  </i>
-                </a>
-              </li>
-              <li>
-                <a
-                  href="https://linkedin.com"
-                  target={"_blank"}
-                  rel="noreferrer"
-                >
-                  <i>
-                    <FontAwesomeIcon icon={faLinkedin} />
-                  </i>
-                </a>
-              </li>
+              {socialMedia.map((media, index) => (
+                <li key={index}>
+                  <a
+                    href={media.href}
+                    target={"_blank"}
+                    rel="noreferrer"
+                  >
+                    <i><FontAwesomeIcon icon={media.icon} /></i>
+                  </a>
+                </li>
+              ))}
             </ul>
           </div>
         </div>
@@ -91,7 +83,7 @@ function Footer() {
       </div>
       <div className="container text-light">
         <div className="row">
-          <div className="col-md-8 col-sm-6 col-xs-12">
+          <div className="col-xs-12 col-sm-6 col-md-8 mx-auto">
             <p className="copyright-text">
               Copyright &copy; 2022 All Rights Reserved
             </p>
